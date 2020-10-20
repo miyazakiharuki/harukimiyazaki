7183b09ef4479acf1ecafdf1d00701e95ec16f47
miyazakiharuki <zm18493@josai.ac.jp>
2020-10-20 02:07:46 +0000

    1020

---
+++ ex06-5.js
@@ -0,0 +1,3 @@
+function sayhello(){var who ='world';alert('Hello,' + who +'!');}
+function taro(){var who ='tarou';alert('太郎が来た');}
+function hanako(){var who ='hanako';alert('花子が来た');}
---
+++ ex06-7.js
@@ -0,0 +1,1 @@
+var who ='world';function sayhello(){alert('Hello,' + who+'!');}function someone(x){who = x;}
